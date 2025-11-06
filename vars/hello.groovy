3c023123fa231fbf2b2138023960746a6ee61b27
ajaysingh3200 <66829002+ajaysingh3200@users.noreply.github.com>
2025-11-06 16:36:18 +0000

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -4,6 +4,6 @@ echo "================================================"
 echo "  Hello India!"
 echo "  Congratulations to the Indian Women's Cricket Team"
 echo "  for winning the 2025 World Cup Championship!"
-echo " Thanks..."!
+echo " Thanks...!"
 echo "================================================"
 }
